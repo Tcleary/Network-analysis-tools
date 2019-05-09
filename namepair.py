62dcc3e0fc2d3568aa0fcc4d534354d35f8c281a
Thomas Cleary <thomas.cleary1@gmail.com>
2019-05-08 22:18:28 -0400

    code cleaned up

--- namepair.py
+++ namepair.py
@@ -1,12 +1,14 @@
+#Code to take a edge file, create a dictionary where the column B is used as a key and column A are used as values.  The dictionary is exported as a CSV with each line holding a different key:value set.
+
 import csv
 
-#have the user input a file
+#Have the user input a file
 filename = input('Enter file name: ')
 
-# set up empty dictionary
+#Set up an empty dictionary
 mydict = {}
 
-#Process the file
+#Open the file
 with open(filename) as r:
     reader = csv.reader(r)  
     # skip header row
@@ -15,17 +17,13 @@ with open(filename) as r:
     for rows in reader:   
         k = rows[1] #sets key value according to fields in Column B
         v = rows[0] #sets values according to fields in Column A
-
-        #lables the key, this can be changed for other purposes
-        workshop = k 
        
-        # create/get key in/from dict if needed, prepopulate value with empty list
-        key = mydict.setdefault(workshop,[])
+        # Get key ifrom dict if needed, prepopulate value with empty list
+        key = mydict.setdefault(k,[])
 
         # append data to list
         key.append(v)
 
-#print (mydict)
 #Printing the dictionaries in a csv so each key has a different line
 with open('dict.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
